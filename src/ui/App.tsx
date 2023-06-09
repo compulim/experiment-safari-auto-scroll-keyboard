37e04b10f7283ead11c8f256e65c727b932bf5bf
William Wong <compulim@users.noreply.github.com>
2023-06-09 18:48:01 +0000

    Initial commit

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,5 +1,13 @@
+import { Fragment } from 'react';
+
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return <Fragment>
+    <div className="upper" tabIndex={0}>
+      <h1 className="item">Hello</h1>
+      <h1 className="item">World</h1>
+    </div>
+    <input className="input-box" placeholder="Type a message" type="text" />
+  </Fragment>;
 };
 
 export default App;
