5e48f99b25ffac2980166aac7af61a95846d3145
William Wong <compulim@users.noreply.github.com>
2023-06-12 21:24:57 +0000

    Mimic Web Chat

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
 
-import App from './ui/App';
+// import App from './ui/App';
+import App from './ui/MimicWebChat';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('webchat');
 
 rootElement && createRoot(rootElement).render(<App />);
